3b71c8f2691f3339e1e4f60defd88ede4f112fab
Roger Spratley <roger_spratley@hotmail.com>
2018-03-03 09:35:56 -0500

    removing JS file

--- Global.Banking/Global.Banking.TypeScript/src/accounttype.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AccountType;
-(function (AccountType) {
-    AccountType[AccountType["Checking"] = 0] = "Checking";
-    AccountType[AccountType["Savings"] = 1] = "Savings";
-    AccountType[AccountType["COD"] = 2] = "COD";
-})(AccountType = exports.AccountType || (exports.AccountType = {}));
-//# sourceMappingURL=accounttype.js.map
--- Global.Banking/Global.Banking.TypeScript/src/transactionresult.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TransactionType;
-(function (TransactionType) {
-    TransactionType[TransactionType["Deposit"] = 1] = "Deposit";
-    TransactionType[TransactionType["Withdrawal"] = 2] = "Withdrawal";
-})(TransactionType = exports.TransactionType || (exports.TransactionType = {}));
-//# sourceMappingURL=transactionresult.js.map
