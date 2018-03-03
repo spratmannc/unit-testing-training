ce0d45b74b3e33b782bacd17126db101589d1698
Roger Spratley <roger_spratley@hotmail.com>
2018-03-03 11:41:51 -0500

    Added C# tests (one failure)

---
+++ Global.Banking/Global.Banking.CSharp.Tests/AccountManagerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Global.Banking.CSharp.Tests
+{
+    public class AccountManagerTests
+    {
+        [Fact(DisplayName = "C# - Account Manager should increase account balances on deposit")]
+        [Trait("Environment", "C#")]
+        [Trait("Class", "AccountManager")]
+        public void ShouldIncreaseOnDeposit()
+        {
+            // Arrange
+            var manager = new AccountManager("accounts.json");
+            var account = manager.Load("11");
+            var expected = account.Balance + 100;
+
+            // Act
+            var transaction = manager.Deposit(account, 100);
+
+            // Assert
+            Assert.True(transaction.Succeeded);
+            Assert.Equal(expected, account.Balance);
+        }
+
+        [Fact(DisplayName = "C# - Account Manager should decrease account balances on withdrawal")]
+        [Trait("Environment", "C#")]
+        [Trait("Class", "AccountManager")]
+        public void ShouldDecreaseOnWithdrawal()
+        {
+            // Arrange
+            var manager = new AccountManager("accounts.json");
+            var account = manager.Load("11");
+            var expected = account.Balance - 100;
+
+            // Act
+            var transaction = manager.Withdraw(account, 100);
+
+            // Assert
+            Assert.True(transaction.Succeeded);
+            Assert.Equal(expected, account.Balance);
+        }
+
+        [Fact(DisplayName = "C# - Account Manager should decrease account balances on withdrawal")]
+        [Trait("Environment", "C#")]
+        [Trait("Class", "AccountManager")]
+        [Trait("Category", "BugFix")]
+        public void ShouldGenerateUniqueTransactionIds()
+        {
+            // Arrange
+            var manager = new AccountManager("accounts.json");
+            var account = manager.Load("11");
+
+            // Act
+            var withdrawal = manager.Withdraw(account, 100);
+            var deposit = manager.Deposit(account, 100);
+
+            // Assert
+            Assert.True(withdrawal.Succeeded);
+            Assert.True(deposit.Succeeded);
+            Assert.NotEqual(withdrawal.Id, deposit.Id);
+        }
+    }
+}
